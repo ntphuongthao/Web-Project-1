56ba2db57310f91d1a4490b54feae66690eb4a68
Tri Nguyen <tringuyen_2023@depauw.edu>
2023-09-29 09:57:35 -0400

    Wrote merch in merch.js

--- server/merch.js
+++ server/merch.js
@@ -1,7 +1,32 @@
 export default merch = [
   {
-    title: "",
-    description: "",
-    image: ""
+    title: "Black DePauw Cup",
+    description: "Black Cup with DePauw logo",
+    image: "cup1.png"
+  },
+  {
+    title: "Grey DePauw Jacket",
+    description: "Grey jacket with zipper and DePauw Logo",
+    image: "jacket1.png"
+  },
+  {
+    title: "Yellow DePauw Hoodie",
+    description: "Yellow hoodie with DePauw Logo",
+    image: "hoodie2.png"
+  },
+  {
+    title: "Black DePauw T-Shirt",
+    description: "Black T-Shirt with DePauw Logo",
+    image: "tee1.png"
+  },
+  {
+    title: "Women DePauw T-Shirt",
+    description: "Grey Women T-Shirt with DePauw Logo",
+    image: "tee2.png"
+  },
+  {
+    title: "DePauw Tumbler",
+    description: "White Tumbler with DePauw's arts",
+    image: "tumbler1.png"
   }
 ];  
