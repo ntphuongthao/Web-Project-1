0878548e122966cde76dbf56c032ea5e20664078
NT Phuong Thao <thao.tp.nguyen02@gmail.com>
2023-09-29 21:55:19 -0400

    Added comment list to each merch

--- server/data.js
+++ server/data.js
@@ -3,36 +3,53 @@ export const merch = [
     id: 0,
     title: "Black DePauw Cup",
     description: "Black Cup with DePauw logo",
-    image: "../assets/cup1.PNG"
+    image: "../assets/cup1.PNG",
+    comments: [
+      "This product is great!!!",
+    ]
   },
   {
     id: 1,
     title: "Grey DePauw Jacket",
     description: "Grey jacket with zipper and DePauw Logo",
-    image: "../assets/jacket1.png"
+    image: "../assets/jacket1.png",
+    comments: [
+      "I recommend you all buy this",
+      "It was normal", 
+    ]
   },
   {
     id: 2,
     title: "Yellow DePauw Hoodie",
     description: "Yellow hoodie with DePauw Logo",
-    image: "../assets/hoodie2.png"
+    image: "../assets/hoodie2.png",
+    comments: [
+      "I would buy this again!",
+    ]
   },
   {
     id: 3,
     title: "Black DePauw T-Shirt",
     description: "Black T-Shirt with DePauw Logo",
-    image: "../assets/tee1.png"
+    image: "../assets/tee1.png",
+    comments: [
+      ":D :D :D",
+    ]
   },
   {
     id: 4,
     title: "Women DePauw T-Shirt",
     description: "Grey Women T-Shirt with DePauw Logo",
-    image: "../assets/tee2.png"
+    image: "../assets/tee2.png",
+    comments: [
+      "I can't find better prices anywhere"
+    ],
   },
   {
     id: 5,
     title: "DePauw Tumbler",
     description: "White Tumbler with DePauw's arts",
-    image: "../assets/tumbler1.PNG"
+    image: "../assets/tumbler1.PNG",
+    comments: [],
   }
 ];  
--- src/components/Merch.vue
+++ src/components/Merch.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="grid-container">
-    <div v-for="merch in merchList" :key="merch.id">
+    <div class="merch-container" v-for="merch in merchList" :key="merch.id">
       <div class="card" style="width: 18rem;">
         <img :src="merch.image" class="card-img-top" :alt="merch.title">
         <div class="card-body">
@@ -8,13 +8,21 @@
           <p class="card-text">{{merch.description}}</p>
         </div>
       </div>  
+      <div class="comment-list">
+        <div v-for="comment in merch.comments" :key="comment">
+          <div class="comment">
+            <img class="user-profile" src="../assets/anonymous-user.jpeg" alt="Anonymous User">
+            <p style="text-align: left;">{{ comment }}</p>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
   <br/>
 </template>
 
 <script>
-import { merch } from "../../server/merch.js";
+import { merch } from "../../server/data.js";
 
 export default {
   name: "Merch",
@@ -30,7 +38,22 @@ export default {
 .grid-container {
   margin: 3rem;
   display: grid;
-  grid-template-columns: repeat(5, 1fr);
-  grid-gap: 10px;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 15px;
+}
+
+.merch-container {
+  display: grid;
+  grid-template-columns: 2fr 3fr;
+  grid-gap: 5px;
+}
+
+.user-profile {
+  width: 40px;
+}
+
+.comment {
+  display: grid;
+  grid-template-columns: 1fr 8fr;
 }
 </style>
